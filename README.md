cbe187df961197681c4b6155b56c3ae50c4370d0
tasso <tasso@raange.com>
2020-05-03 12:53:42 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+## Graal VM
+### Mac Os
+Run: `graalvm_macos.sh` to initialize the environment for Graal.
+
+*Note:* Currently setup for `20.0.0`
+
---
+++ graalvm_macos.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export PATH=/Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/bin:$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home
+export GRAALVM_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home
+
+echo "Path: $PATH"
+echo "Java Home: $JAVA_HOME"
+echo "Graal Home: $GRAALVM_HOME"
+
